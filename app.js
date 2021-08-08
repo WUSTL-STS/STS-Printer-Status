88ab848f262d9143f14c4a87ed31ac3de3e7a1da
jackheuberger <jackheuberger@wustl.edu>
2021-08-08 17:41:58 -0400

    Fixed merge conflict, added Bootstrap icons

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ connection.once('open', () => {
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/icons', express.static(__dirname + '/node_modules/bootstrap-icons'));
 
 //Enable handlebars
 app.engine('.hbs', exphbs({
