cbbcd565a37c947f1d0d8ac6ab752457fea3daa9
Jack Heuberger <jackheuberger5@gmail.com>
2024-02-09 12:10:48 -0600

    Finished

--- scripts/generateReport.js
+++ scripts/generateReport.js
@@ -3,14 +3,14 @@ const logger = require('../scripts/logger')
 const fs = require('fs')
 
 async function generateReport () {
-    const transport = await nodemailer.createTransport({
-        host: 'outlook.office365.com',
+    const transport = nodemailer.createTransport({
+        host: 'rsmtp.mx.wustl.edu',
         port: 587,
-        secure: false,
-        auth: {
-            user: 'student.technology@go.wustl.edu',
-            pass: process.env.EMAIL_PASS
-        }
+        secure: false
+        // auth: {
+        //     user: 'student.technology@go.wustl.edu',
+        //     pass: process.env.EMAIL_PASS
+        // }
     })
     const msg = await transport.sendMail({
         from: 'student.technology@wustl.edu',
