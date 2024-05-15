4911988bf1ebf9d93db82cbb98d88251c977daff
taihuynh132 <taih@wustl.edu>
2024-05-15 12:51:11 -0500

    updating error messages for getError and removing paper function

--- scripts/getError.js
+++ scripts/getError.js
@@ -15,15 +15,13 @@ async function getError () {
                 //checkpoint1
                 logger.info(session = new snmp.Session({ host: printers[i].url, timeouts: [8000] })) // Create new SNMP session
                 session = new snmp.Session({ host: printers[i].url, timeouts: [8000] })
-                logger.info('Past Checkpoint 1')
+                logger.info('Past Error Checkpoint 1')
                 //checkpoint2
                 const toner = await fetchToner(printers[i].location)
-                logger.info('Past Checkpoint 2')
+                logger.info('Past Error Checkpoint 2')
                 //checkpoint3
-                const paper = await fetchPaper(printers[i].location)
-                logger.info('Past Checkpoint 3')
+                logger.info('Past Error Checkpoint 3')
                 printers[i].set('toner', toner)
-                printers[i].set('paper', paper)
                 await printers[i].save()
                 logger.info('successfully queried ' + printers[i].location)
             } catch (err) {
@@ -46,15 +44,15 @@ function fetchToner (location) {
 
     return new Promise((resolve, reject) => {
 
-        logger.info('creating new array')
+        logger.info('creating new array for error')
 
         const toner = new Array(8)
-        logger.info('getting subtree')
+        logger.info('getting subtree for error')
 
         logger.info(
         session.getSubtree({ oid: [1, 3, 6, 1, 2, 1, 43, 11, 1, 1, 9, 1]}, function (error, varbinds) {
             
-            logger.info('subtree generated')
+            logger.info('error subtree generated')
 
             // If error, reject the promise
             if (error) {
@@ -72,25 +70,6 @@ function fetchToner (location) {
     })
 }
 
-// Returns an array of length 4 representing paper tray fill
-function fetchPaper (location) {
-    return new Promise((resolve, reject) => {
-        const paper = new Array(4)
-        session.getSubtree({ oid: [1, 3, 6, 1, 2, 1, 43, 8, 2, 1, 10]}, (error, varbinds) => {
-            if (error) {
-                logger.error("error fetching paper for " + location + " " + error)
-                reject(error)
-            } else {
-                // 5 total tray varbinds, first is for bypass -- can be ignored
-                for (let i = 1; i < varbinds.length; i++) {
-                    // eslint-disable-next-line eqeqeq
-                    paper[i - 1] = varbinds[i].value == '-3'
-                }
-                logger.info(`paper fetch for ${location} successful: ${paper}`)
-                resolve(paper)
-            }
-        })
-    })
-}
+
 
 module.exports = getError
